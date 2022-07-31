4ade847eba0940d8120c1567dbb0ccf9335da3c7
Paulo Moral <yorxk@hotmail>
2022-07-31 03:34:31 -0300

    Subindo o teste

---
+++ README.md
@@ -0,0 +1,66 @@
+# Q.A Challenge 20200715 - Paulo Moral
+
+Por favor, complete o seguinte desafio para a sua entrevista online. Terá de documentar o seu processo e apresentá-lo na entrevista (utilizando um PowerPoint ou apresentação semelhante), bem como demonstrar o resultado durante a entrevista técnica.
+
+Os resultados destas tarefas são menos importantes do que o seu processo de pensamento e decisões à medida que as completa, por isso tente documentar e apresentar as suas hipóteses e decisões na medida do possível.
+
+## Desafio
+
+Neste desafio aplicaremos os conceitos de teste caixa preta, onde testaremos a página Beta da [Coodesh](https://beta.coodesh.com/) em vários aspectos como por exemplo:
+
+- Se a home carrega correcta
+- Se a página de vagas tem resultados
+- Se ao utilizar o buscar, obtemos o resultado esperado ou não
+- Se o botão candidatar nas vagas abrem o modal de candidatura
+
+## Parte 1 - Escolha do Frawework
+
+Para realizar o teste podemos escolher entre os 3 frameworks de teste:
+
+- Cucumber
+- Selenium
+- Ghost Inspect
+
+Explicar o por quê da escolha, as vantagens e desvantagens dos que não foram escolhidos.
+
+<P.M.: Escolhi o Selenium para realizar o teste pois nele consigo facilmente, com poucas linhas de código, realizar o que foi solicitado. O Cucumber realizar uma função um pouco mais abrangente, arquivando e relatando os testes realizados. Quanto ao Ghost Inspect confesso que nunca usei mas sei que tem um papel intermediário entre Cucumber e Selenium, onde realiza as duas funções.>
+
+
+## Parte 2 - Mãos à Obra
+
+Nesse parte teremos que desenvolver um projeto para testar vários aspectos da página Beta da Coodesh:
+
+- https://beta.coodesh.com/
+
+
+Testes para aplicar:
+
+1- Se a página está carregando corretamente a home page;
+
+2- Navegar pela página e ir ao sistema de vagas no menú superior;
+
+3- Um vez na tela de vagas, nosso sistema de teste automatizado terá que buscar por `uma empresa` onde terá um ou mais resultados de vagas;
+
+4- Depois obter o resultado, deveremos abrir a vaga e revisar se o carregamento é realizado com sucesso ou não;
+
+5- Por último, precisamos saber se o sistema de candidatar está funcionando e para isso é necessário clicar no botão `Candidatar-se` e revisar se abriu o modal corretamente.
+
+## Finalização
+
+Avisar sobre a finalização e enviar para correção em: [https://coodesh.com/review-challenge](https://coodesh.com/review-challenge)
+Após essa etapa será marcado a apresentação/correção do projeto.
+
+## Instruções para a Apresentação:
+
+1. Será necessário compartilhar a tela durante a vídeo chamada;
+2. Deixe todos os projetos de solução previamente abertos em seu computador antes de iniciar a chamada;
+3. Deixe os ambientes configurados e prontos para rodar;
+4. Prepara-se pois você será questionado sobre cada etapa e decisão do Challenge;
+5. Prepare uma lista de perguntas, dúvidas, sugestões de melhorias e feedbacks (caso tenha).
+
+## Suporte
+
+Use a [nossa comunidade](https://coodesh.com/desenvolvedores#community) para tirar dúvidas sobre o processo ou envie um e-mail para contato@coodesh.com.
+
+
+
---
+++ index.js
@@ -0,0 +1,44 @@
+const  {Builder, By, Key, unitl} = require ("selenium-webdriver");
+const assert = require ("assert");
+
+async function example() {
+    let driver = await new Builder().forBrowser("chrome").build(); // abrindo Chrome
+    await driver.get("https://beta.coodesh.com/"); // indo para o sistema
+    await driver.sleep(4000); // aguardando 4 segundos para localizar o elemento dos Cookies
+    await driver.findElement(By.id("onetrust-accept-btn-handler")).click(); // clicando no OK dos Cookies
+    let confirmHome = await driver.findElement(By.css("#content > div > div > div > div.text-center.text-lg-left.space-top-2.col-lg-6 > div > span")).getText().then(function(value) {
+        return value
+    });
+    assert.strictEqual(confirmHome, "Conectamos\ndevelopers validados\nàs empresas"); // 1 - Se a página está carregando corretamente a home page
+    await driver.sleep(2000);
+    await driver.executeScript("window.alert('Home ok!')");
+    await driver.sleep(2000);
+    await driver.switchTo().alert().accept();
+    await driver.findElement(By.className("u-hamburger__inner")).click(); // Clicando no Menu
+    await driver.findElement(By.className("nav-link u-header__nav-link")).click(); // 2 - Navegar pela página e ir ao sistema de vagas no menú superior
+    await driver.sleep(4000);
+    let companyName = await driver.findElement(By.css("#content > div > div:nth-child(3) > div:nth-child(1) > a > div.p-3 > div.d-lg-flex.justify-content-between.px-2.row > div.d-flex.align-items-center.pr-0.col-xl-5.col-lg-6 > div.pr-4.mb-3.mr-1.pt-1 > ul > li:nth-child(1) > span")).getText();
+    await driver.findElement(By.name("search")).sendKeys(companyName); // com o texto do nome da empresa salvo na variavel, jogo ele no search
+    await driver.findElement(By.className("jsx-2868889731 fas fa-search btn-icon__inner")).click(); // 3 - Um vez na tela de vagas, nosso sistema de teste automatizado terá que buscar por `uma empresa` onde terá um ou mais resultados de vagas
+    await driver.manage().window().maximize();
+    await driver.sleep(4000);
+    let confirmPage = await driver.findElement(By.css("#content > div > div > div.mb-lg-0.d-none.d-lg-block.col-lg-5 > nav > div.m-0.row > button")).getText().then(function(value){
+        return value
+    }); // confirmando que a vaga abriu copiando um texto fixo da mesma
+    assert.strictEqual(confirmPage, "Tenho Interesse"); // 4 - Depois obter o resultado, deveremos abrir a vaga e revisar se o carregamento é realizado com sucesso ou não
+    await driver.sleep(2000);
+    await driver.executeScript("window.alert('Vaga ok!')");
+    await driver.sleep(2000);
+    await driver.switchTo().alert().accept();
+    await driver.findElement(By.css("#content > div > div > div.mb-lg-0.d-none.d-lg-block.col-lg-5 > nav > div.m-0.row > button")).click(); // 5.1 - Por último, precisamos saber se o sistema de candidatar está funcionando e para isso é necessário clicar no botão `Candidatar-se`...
+    await driver.sleep(4000);
+    let confirmModal = await driver.findElement(By.css("body > div:nth-child(34) > div > div > div.modal-body > div > div > div > div.text-center.space-bottom-1 > h2")).getText().then(function(value) {
+        return value
+    });
+    assert.strictEqual(confirmModal, "Faça login"); // 5.2 - e revisar se abriu o modal corretamente
+    await driver.sleep(2000);
+    await driver.executeScript("window.alert('Modal ok! All Tests passed ok!')");
+    await driver.sleep(2000);
+    await driver.quit();
+};
+example();
